ec95bbdb7ed236d9946e0b0516b7bf4b62b9fab6
Francisco Di Giandomenico <francisco.digiandomenico@gmail.com>
2024-08-13 16:13:24 -0400

    feat: make it all work because I'm awesome

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log({ moment });
+  const date = generateRandomDate();
+  appendToTimeElement(date);
+});
+
+/**
+ * Generates a random date between tomorrow and 2 years from now
+ * @returns
+ */
+function generateRandomDate() {
+  const tomorrow = moment().add(1, "days");
+  const twoYearsFromNow = moment().add(2, "years");
+  const randomDate = moment(
+    Math.floor(Math.random() * (twoYearsFromNow - tomorrow + 1)) + tomorrow
+  );
+  return randomDate;
+}
+
+function appendToTimeElement(date) {
+  const daysFromNow = moment(date).diff(moment(), "days");
+  document.getElementById("time").innerHTML = `${moment(
+    date
+  ).fromNow()} (${daysFromNow} days)`;
+}
